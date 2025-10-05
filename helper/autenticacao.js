1e013208de6de7af9b4b907d5987dc147636b75e
celiapaivab <celiadepaivabruno@gmail.com>
2025-10-05 20:18:51 -0300

    First transfer test added

--- helper/autenticacao.js
+++ helper/autenticacao.js
@@ -1,14 +1,13 @@
 const request = require('supertest')
+const postLogin = require('../fixtures/postLogin.json');
 
-const obterToken = async (username, password, favorecidos) => {
+const obterToken = async (username, password) => {
+    const bodyLogin = { ...postLogin }
+    
     const respostaLogin = await request('http://localhost:3000')
-        .post('/users/register')
+        .post('/users/login')
         .set('Content-Type', 'application/json')
-        .send({
-            "username": username,
-            "password": password,
-            "favorecidos": favorecidos
-        })
+        .send(bodyLogin)
     return respostaLogin.body.token 
 }
 
